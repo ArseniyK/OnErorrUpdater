4129b01e5b269cfc2a9f081aefc3ae76f1b516b6
Arseniy Krasnov <arseniy@krasnoff.org>
2014-06-05 14:17:37 +0400

    Update background.js

--- background.js
+++ background.js
@@ -2,5 +2,5 @@ chrome.webNavigation.onErrorOccurred.addListener(function(details)
 {
     if (details.frameId != 0) //ignore subframes. 0 is main frame
     { return; }
-    chrome.tabs.update(details.tabId, {url:details.url});    
+    setTimeout(function () {chrome.tabs.update(details.tabId, {url:details.url})}, 30000);    
 });
